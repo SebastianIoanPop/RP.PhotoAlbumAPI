c70952c0221685aad3ea1738a7a3aedf381b9472
Ve Mac <vemac@ves-macbook-pro.home>
2019-11-17 18:02:06 +0000

    Fix issue with solution, testing on different machine

---
+++ RP.PhotoAlbum.Provider.Feature.Tests.Unit/Extensions/PhotoFilterTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using RP.PhotoAlbum.Provider.Feature.Domain;
+using RP.PhotoAlbum.Provider.Feature.Extensions;
+using Shouldly;
+
+namespace RP.PhotoAlbum.Provider.Feature.Tests.Unit.Extensions
+{
+    public class PhotoFilterTests
+    {
+        public void FilterPhotos_Always_ShouldGroupPhotosToAlbums()
+        {
+            // Arrange
+            var photos = new List<Photo>
+            {
+                new Photo
+                {
+                    AlbumId = 1,
+                    Id = 1
+                },
+                new Photo
+                {
+                    AlbumId = 1,
+                    Id = 2
+                },
+                new Photo
+                {
+                    AlbumId = 2,
+                    Id = 3
+                },
+                 new Photo
+                {
+                    AlbumId = 1,
+                    Id = 4
+                }
+            };
+
+            var albums = new List<Album>
+            {
+                new Album
+                {
+                    Id = 1,
+                },
+                new Album
+                {
+                    Id = 2
+                }
+            };
+
+            // Act
+            foreach (var album in albums)
+            {
+                album.FilterPhotos(photos);
+            }
+
+            // Assert
+            foreach (var album in albums)
+            {
+                album.Photos.ShouldNotContain(photo => photo.AlbumId != album.Id);
+            }
+        }
+    }
+}
---
+++ RP.PhotoAlbumAPI.Tests.Unit/Controllers/PhotoAlbumControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NSubstitute;
+using RP.PhotoAlbum.Provider.Feature.Service.Interface;
+using RP.PhotoAlbumAPI.Controllers;
+using Xunit;
+
+namespace RP.PhotoAlbumAPI.Tests.Unit.Controllers
+{
+    public class PhotoAlbumControllerTests
+    {
+        private IPhotoAlbumService _mockPhotoAlbumService;
+        private PhotoAlbumController _subject;
+
+        public PhotoAlbumControllerTests()
+        {
+            _mockPhotoAlbumService = Substitute.For<IPhotoAlbumService>();
+
+            _subject = new PhotoAlbumController(_mockPhotoAlbumService);
+        }
+
+        [Fact]
+        public void Get_Always_CallsTheService()
+        {
+            // Act
+            _ = _subject.Get(null);
+
+            // Assert
+            _mockPhotoAlbumService.Received(1).GetAsync(null);
+        }
+
+        [Fact]
+        public void Get_Always_CallsTheServiceWithCorrectParameter()
+        {
+            // Arrange
+            var randomInt = new Random().Next();
+
+            // Act
+            _ = _subject.Get(randomInt);
+
+            // Assert
+            _mockPhotoAlbumService.Received(1).GetAsync(randomInt);
+        }
+    }
+}
